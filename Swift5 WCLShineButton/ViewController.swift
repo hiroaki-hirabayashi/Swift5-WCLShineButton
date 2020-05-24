c89331b6728dce708029ae1d70981f4dc08fb356
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-24 21:12:14 +0900

    Initial Commit

---
+++ Swift5 WCLShineButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5 WCLShineButton
+//
+//  Created by 平林宏淳 on 2020/05/24.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
